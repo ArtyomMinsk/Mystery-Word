642ae732db2d136d2160e79021964a60dfc277bf
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-16 01:54:07 -0400

    Completed functions display_word and is_word_complete.

--- mystery_word.py
+++ mystery_word.py
@@ -1,63 +1,64 @@
 import random
 
-#write a function that asks user to choose a level of difficulty
-def get_secret_word():
-    level = input("Please enter the level of difficulty: ").lower()
-    if level == "easy":
-        print("You chose an easy level!")
-        my_easy_list = get_easy_list()
-
-        my_word = random.choice(my_easy_list)
-        print(my_word)
-        print(" _ " * len(my_word))
-        return my_word
-
-    elif level == "normal":
-        print("You chose a normal level!")
-        my_normal_list = get_normal_list()
-
-        my_word = random.choice(my_normal_list)
-        print(my_word)
-        print(" _ " * len(my_word))
-        return my_word
-
-    elif level == "hard":
-        print("You chose a hard level!")
-        my_hard_list = get_hard_list()
-
-        my_word = random.choice(my_hard_list)
-        print(my_word)
-        print(" _ " * len(my_word))
-        return my_word
-
 #write a function to get a list and a random word if level is easy
-def get_easy_list():
+def easy_words(word_list):
     my_easy_list = []
-    with open('/usr/share/dict/words', 'r') as f:
-        for item in f:
-            if len(item.strip()) >= 4 and len(item.strip()) <= 6:
-                my_easy_list.append(item.strip())
+    #with open('/usr/share/dict/words', 'r') as f:
+    for item in word_list:
+        if len(item.strip()) >= 4 and len(item.strip()) <= 6:
+            my_easy_list.append(item.strip())
     return my_easy_list
-
+    #print(my_easy_list)
 
 #write a function to get a list and a random word if level is normal
-def get_normal_list():
+def medium_words(word_list):
     my_normal_list = []
-    with open('/usr/share/dict/words', 'r') as f:
-        for item in f:
-            if len(item.strip()) >= 6 and len(item.strip()) <= 8:
-                my_normal_list.append(item.strip())
+    #with open('/usr/share/dict/words', 'r') as f:
+    for item in word_list:
+        if len(item.strip()) >= 6 and len(item.strip()) <= 8:
+            my_normal_list.append(item.strip())
     return my_normal_list
-
+    #print(my_normal_list)
 
 #write a function to get a list and a random word if level is hard
-def get_hard_list():
+def hard_words(word_list):
     my_hard_list = []
-    with open('/usr/share/dict/words', 'r') as f:
-        for item in f:
-            if len(item.strip()) >= 8:
-                my_hard_list.append(item.strip())
+    #with open('/usr/share/dict/words', 'r') as f:
+    for item in word_list:
+        if len(item.strip()) >= 8:
+            my_hard_list.append(item.strip())
     return my_hard_list
+    #print(my_hard_list)
 
+#write a function that asks user to choose a level of difficulty
+def random_word(word_list, level):
+    filtered_word_list = []
+    my_word = ""
 
-get_secret_word()
+    if level == "easy":
+        filtered_word_list = easy_words(word_list)
+        my_word = random.choice(filtered_word_list)
+
+    elif level == "normal":
+        filtered_word_list = medium_words(word_list)
+        my_word = random.choice(filtered_word_list)
+
+    elif level == "hard":
+        filtered_word_list = hard_words(word_list)
+        my_word = random.choice(filtered_word_list)
+
+    print(my_word)
+    print(" _ " * len(my_word))
+    return my_word
+
+#write a function to guess a word
+#def display_word(word, guesses):
+
+def main():
+    level = input("Please enter the level of difficulty: ").lower()
+
+    with open('/usr/share/dict/words', 'r') as word_list:
+        random_word(word_list, level)
+
+if __name__ == '__main__':
+    main()
