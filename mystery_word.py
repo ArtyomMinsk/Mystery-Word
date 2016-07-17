a1b1824fd587a7af78e562eebe896e4f76ca163f
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-17 15:18:36 -0400

    Added while loop to the main function to ask user if he wants to paly again

--- mystery_word.py
+++ mystery_word.py
@@ -1,7 +1,7 @@
 import random
 #level = ""
 
-#write a function to get a list and a random word if level is easy
+# Write a function to get a list and a random word if level is easy
 def easy_words(word_list):
     my_easy_list = []
     #with open('/usr/share/dict/words', 'r') as f:
@@ -11,7 +11,7 @@ def easy_words(word_list):
     return my_easy_list
     #print(my_easy_list)
 
-#write a function to get a list and a random word if level is normal
+# Write a function to get a list and a random word if level is normal
 def medium_words(word_list):
     my_normal_list = []
     #with open('/usr/share/dict/words', 'r') as f:
@@ -21,7 +21,7 @@ def medium_words(word_list):
     return my_normal_list
     #print(my_normal_list)
 
-# write a function to get a list and a random word if level is hard
+# Write a function to get a list and a random word if level is hard
 def hard_words(word_list):
     my_hard_list = []
     #with open('/usr/share/dict/words', 'r') as f:
@@ -31,13 +31,13 @@ def hard_words(word_list):
     return my_hard_list
     #print(my_hard_list)
 
-# Returns a random word from the easy/normal/hard list
+# Return a random word from the easy/normal/hard list
 def random_word(word_list):
     my_word = ""
     my_word = random.choice(word_list)
     return my_word
 
-# Returns a string that including blanks (_) and letters from the given word
+# Return a string that including blanks (_) and letters from the given word
 def display_word(word, guesses):
     secret_word_list = list(word)
     #print(secret_word_list)
@@ -70,7 +70,6 @@ def is_word_complete(word, guesses):
     for letter in range(len(secret_word_list)):
         guessed_word_list.append("_")
 
-
     for letter in guesses:
         for index in range(len(secret_word_list)):
             if letter == secret_word_list[index]:
@@ -79,69 +78,84 @@ def is_word_complete(word, guesses):
 
     display = ''.join(guessed_word_list)
     if word == display:
-        #print("Yes")
+        print(' '.join(display.upper()))
         return True
     else:
-        #print("No")
         return False
 
 def main():
-    # Prompt the user for a difficulty level
-    level = input("Please enter the level of difficulty (easy/normal/hard): ").lower()
-    short_list = []
-
-    # Set up the game based upon the difficulty level
-    with open('/usr/share/dict/words', 'r') as word_list:
-        if level == "easy":
-            short_list = easy_words(word_list)
-
-        elif level == "normal":
-            short_list = medium_words(word_list)
-
-        elif level == "hard":
-            short_list = hard_words(word_list)
-
-        word = random_word(short_list).lower()
-        print(word)
-
-    secret_word_list = list(word)
-    print(secret_word_list)
-
-    guess = 0
-
-    # this list stores all previous guesses
-    letters_guessed_list = []
-
-    # Perform the game loop
     while True:
-        # Displaying whether the user has won or lost
-        if guess < 8 and is_word_complete(word, letters_guessed_list):
-            print("Congratulations! You guessed the word!")
-            return True
-        elif guess == 8 and (not is_word_complete(word, letters_guessed_list)):
-            print("You lost!")
-            return True
+        # Prompt the user for a difficulty level
+        level = input("Please enter the level of difficulty (easy/normal/hard): ").lower()
+        short_list = []
 
-        # Printing the number of guesses remaining
-        print("You have {} guesses left".format(8 - guess))
+        # Set up the game based upon the difficulty level
+        with open('/usr/share/dict/words', 'r') as word_list:
+            if level == "easy":
+                short_list = easy_words(word_list)
 
-        display_word(word, letters_guessed_list)
+            elif level == "normal":
+                short_list = medium_words(word_list)
 
-        # Prompting the user for a letter to guess
-        input_letter = input("Please guess a letter: ").lower()
+            elif level == "hard":
+                short_list = hard_words(word_list)
 
-        # check if user guessed a letter before
-        if input_letter in letters_guessed_list:
-            print("You have already guessed this letter! ")
+            word = random_word(short_list).lower()
+            #print(word)
+
+        secret_word_list = list(word)
+
+        # The number of quesses
+        guess = 0
+
+        # This list stores all previous guesses
+        letters_guessed_list = []
+
+        # Perform the game loop
+        while True:
+            # Display whether the user has won or lost
+            if guess < 8 and is_word_complete(word, letters_guessed_list):
+                print("Congratulations! You guessed the word!")
+                # Give the user the option to play again
+                answer = input("Would you like to play again? [Y/n] ").lower()
+                break
+
+            elif guess == 8 and not is_word_complete(word, letters_guessed_list):
+                print("You lost!")
+                print("The secret word was: ", word.upper())
+                # Give the user the option to play again
+                answer = input("Would you like to play again? [Y/n] ").lower()
+                break
+
+            # Print the number of guesses remaining
+            print("You have {} guesses left".format(8 - guess))
+
+            display_word(word, letters_guessed_list)
+
+            # Prompt the user for a letter to guess
+            input_letter = input("Please guess a letter: ").lower()
+
+            # Check for invalid input
+            if len(input_letter) > 1:
+                print("The input is invalid! Please try again!")
+                input_letter = input("Please guess a letter: ").lower()
+
+            # Check if user guessed a letter before
+            if input_letter in letters_guessed_list:
+                print("You have already guessed this letter! Please try again! ")
+                continue
+            else:
+                # Print the letters that have been guessed so far
+                letters_guessed_list.append(input_letter)
+                print("Letters that have been guessed so far: {}".format(letters_guessed_list))
+
+            if input_letter not in secret_word_list:
+                 guess += 1
+
+        if answer == "y":
             continue
-        else:
-            letters_guessed_list.append(input_letter)
-            print("Letters guessed list: {}".format(letters_guessed_list))
-
-
-
-        if input_letter not in secret_word_list:
-             guess += 1
+        elif answer == "n":
+            break
 
 if __name__ == '__main__':
     main()
